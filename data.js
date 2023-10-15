88bba6d6b6576cd40b26529984136f005d13e646
Rewan Hamed <77508295+rewanhamed22@users.noreply.github.com>
2023-10-15 06:47:03 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,46 @@
+let songs = [
+  {
+    name: "Mesh Shayfenhom",
+    path: "./musics/Hassan El Shafie ft. Pousi & Double Zuksh - Mesh Shayfenhom (Coke Studio Egypt 2023).mp3",
+    artist: "Bosy",
+    cover:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGLZtggTn8NsB1JZXhTelJ7VDMfp58Tb6hUg&usqp=CAU",
+  },
+  {
+    name: "Eldalaa",
+    path: "./musics/El Dalaa.mp3",
+    artist: "Bahaa Soltan",
+    cover:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa5DtTfY19Lfi7Iix2ATHEVqAEizyvoMX6CA&usqp=CAU",
+  },
+  {
+    name: "song 1",
+    path: "musics/1.mp3",
+    artist: "artist 1",
+    cover: "./images/1.png",
+  },
+  {
+    name: "song 2",
+    path: "musics/2.mp3",
+    artist: "artist 2",
+    cover: "./images/2.png",
+  },
+  {
+    name: "song 3",
+    path: "musics/3.mp3",
+    artist: "artist 3",
+    cover: "./images/3.png",
+  },
+  {
+    name: "song 4",
+    path: "musics/4.mp3",
+    artist: "artist 4",
+    cover: "./images/4.png",
+  },
+  {
+    name: "song 5",
+    path: "musics/5.mp3",
+    artist: "artist 5",
+    cover: "./images/5.png",
+  },
+];
---
+++ script.js
@@ -0,0 +1,83 @@
+let currentMusic = 0;
+const music = document.querySelector("#audio");
+const playBtn = document.querySelector(".play-btn");
+const disk = document.querySelector(".disk");
+const bar = document.querySelector(".bar");
+const songName = document.querySelector(".music-name");
+const artist = document.querySelector(".artist-name");
+const currentTime = document.querySelector(".current-time");
+const duration = document.querySelector(".duration");
+const forward = document.querySelector(".forward-btn");
+const backward = document.querySelector(".backward-btn");
+
+playBtn.addEventListener("click", () => {
+  if (playBtn.className.includes("pause")) {
+    music.play();
+  } else {
+    music.pause();
+  }
+  playBtn.classList.toggle("pause");
+  disk.classList.toggle("play");
+});
+
+const playMusic = () => {
+  music.play();
+  playBtn.classList.remove("pause");
+  disk.classList.add("play");
+};
+
+const setMusic = (i) => {
+  bar.value = 0;
+  let song = songs[i];
+  currentMusic = i;
+  music.src = song.path;
+  songName.innerHTML = song.name;
+  artist.innerHTML = song.artist;
+  disk.style.backgroundImage = `url(${song.cover})`;
+  currentTime.innerHTML = "00:00";
+  setTimeout(() => {
+    bar.max = music.duration;
+    console.log(music.duration);
+    duration.innerHTML = formatTime(music.duration);
+  }, 300);
+};
+setMusic(1);
+const formatTime = (time) => {
+  let min = Math.floor(time / 60);
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  let sec = Math.floor(time % 60);
+  if (sec < 10) {
+    sec = `0${sec}`;
+  }
+  return `${min} : ${sec}`;
+};
+setInterval(() => {
+  bar.value = music.currentTime;
+  currentTime.innerHTML = formatTime(music.currentTime);
+  if (Math.floor(music.currentTime) == Math.floor(bar.max)) {
+    forward.click();
+  }
+}, 500);
+bar.addEventListener("change", () => {
+  music.currentTime = bar.value;
+});
+forward.addEventListener("click", () => {
+  if (currentMusic >= songs.length - 1) {
+    currentMusic = 0;
+  } else {
+    currentMusic++;
+  }
+  setMusic(currentMusic);
+  playMusic();
+});
+backward.addEventListener("click", () => {
+  if (currentMusic <= 0) {
+    currentMusic = songs.length - 1;
+  } else {
+    currentMusic--;
+  }
+  setMusic(currentMusic);
+  playMusic();
+});
